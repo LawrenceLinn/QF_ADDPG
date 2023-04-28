95be26f2a540e4a12385d6bf957362d6415cc7ff
林北 <jeremylinrs@foxmail.com>
2023-04-28 16:29:19 +0800

    first commit

---
+++ Environment/portfolio_share.py
@@ -0,0 +1,71 @@
+"""
+@Author: Runsheng Lin
+Inspired by Qiu.
+"""
+import numpy as np
+
+class Portfolio(object):
+
+    def __init__(self,**port_param):
+        """
+           steps
+           cost
+           mode
+           eta
+        """
+        super(Portfolio, self).__init__()
+        for key, value in port_param.items():
+            setattr(self, key, value)
+        self.infos = []
+        self.w_1 = 0
+
+
+    def step(self,w1,y1,reset = 0):
+        """
+        :param w1:
+        :param y1:
+        :param reset:
+        :return: reward, info, done
+        """
+        w0 = self.w0
+        p0 = self.p0
+        y0 = self.y0
+        dw = y0*w0/(y0@w0)
+
+
+        mu1 = self.cost * (np.abs(dw[1:] - w1[1:])).sum()
+
+        p1 = p0 * (1 - mu1) * np.dot(y1, w1)
+
+        rho1 = p1 / p0 - 1
+        r1 = np.log((p1 + 1e-8) / (p0 + 1e-8))
+        self.entropy_loss = self.eta*(1-(w1**2).sum())
+
+        reward = r1*100-self.entropy_loss
+
+        self.w_1 = self.w0
+        self.w0 = w1
+        self.p_1 = self.p0
+        self.p0 = p1
+        self.y0 = y1
+
+        # Run out of money, done
+        done = p1 == 0
+
+        info = {
+            "portfolio_value": p1,
+            "rate_of_return": rho1,
+            "log_return": r1,
+        }
+        self.infos.append(info)
+        return reward, info, done
+
+
+    def reset(self):
+        self.infos = []
+        self.w0 = np.array([1.0] + [0.0] * self.product_num)
+        self.w_1 = np.array([1.0] + [0.0] * self.product_num)
+        self.p0 = 1.0
+        self.p_1 = 1.0
+        self.y0 = np.ones((self.product_num+1,), dtype=float)
+        self.y0[0] = 1
--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-README
+# README
+
+This is the repository of a Final Year Project conducted by Runsheng LIN, Zihan XING, and Mingze MA, and supervised by Dr. Raymond LEE in Dec 2022. 
+
+The detailed usage of the components in the repository is as follows:
+
+#### Data
+
+We selected the 5 most popular forex products in the world as our data, Including the data set for training and the unseen data for testing.
+
+#### Environment
+
+The environment settings for Reinforcement Learning.
+
+#### tools
+
+Some necessary tools to implement the models, such as the data loader and the OU Process in the DDPG model. 
+
+#### models
+
+The model structure of the original DDPG and our model.
+
+#### QF_ADDPG
+
+The code of the implementation of our model
+
+
+
+
+
+
+# QF_ADDPG
